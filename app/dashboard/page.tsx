a6327131a6ecbff03dabe6302cebcf3ab6796724
anis2566 <anichuranis1000@gmail.com>
2024-06-18 20:59:30 +0600

    first

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+// import PlaceholderContent from "@/components/demo/placeholder-content";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator
+} from "@/components/ui/breadcrumb";
+import { ContentLayout } from "@/components/dashboard";
+
+export default function Dashboard() {
+  return (
+    <ContentLayout title="Dashboard">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Dashboard</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      {/* <PlaceholderContent /> */}
+      Hello
+    </ContentLayout>
+  );
+}
